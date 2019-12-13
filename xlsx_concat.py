847245d3db861a65886b7c5193b0a5bece898cec
Keating55 <55493980+Keating55@users.noreply.github.com>
2019-12-13 14:21:59 +0800

    Update xlsx_concat.py

--- xlsx_concat.py
+++ xlsx_concat.py
@@ -1,28 +1,25 @@
 # -*- coding: utf-8 -*-
-#将xls和csv转化为xlsx		Trans(path)
-#将xlsx合并（按列）		xlsx_concat(path_new)
-
-
-
+#1	将xls和csv转化为xlsx		Trans(path)
+#2	将xlsx合并（按列）		xlsx_concat(path_new)
+#3	1、2合并				xlsx_con()
 
 import win32com.client as win32
 import os 
 import pandas as pd 
 import xlrd
 
-
-
 def help():
 	help_o=pd.DataFrame(data=[['xls、csv转化xlsx','Trans(path)','win32,os '],
-		['xlsx合并(按列)','xlsx_concat(path_new)','pd,os,xlrd']],
+		['xlsx合并(按列)','xlsx_concat(path_new)','pd,os,xlrd'],
+		['xls转化合并','xlsx_con','//']],
 		columns=['名称','调用','库'])
 	print(help_o)
     
 def Trans(path):
-'''
-import win32com.client as win32
-import os 
-'''
+	'''
+	import win32com.client as win32
+	import os 
+	'''
 #1	创建Transfrom-xlsx文件夹	
 	path_new=path+'\\Trans'
 	if os.path.exists(path_new):
@@ -51,14 +48,12 @@ import os
 	print('转化结束')
 	path2=pa	
 
-
-
 def xlsx_concat(path_new):
-'''
-import pandas as pd
-import os 
-import xlrd
-'''
+	'''
+	import pandas as pd
+	import os 
+	import xlrd
+	'''
 #1	获取xlsx文件名称列表
 	print('合并目录',path_new)
 	filelist = []
@@ -88,6 +83,16 @@ import xlrd
 			data_last['来源'] = str(filelist[num_file])+'@'+str(sheet_new)
 #4	将datalist的所有DataFrame按列合并
 	data = pd.concat(datalist,axis=0,sort=False)
-	data.to_excel('@excel_output.xls')
-	print('已输出到@excel_output.xls')
+	outwriter = path_new  + '/' + '@excel_output.xls'
+	data.to_excel(outerwriter)
+	print('已输出到Trans/@excel_output.xls')
 	print(data.shape)
+
+def xlsx_con（）:
+	path=os.getcwd()
+	path_new=path+'\\Trans'
+	print('=====第一步=====')
+	Trans(path)
+	print('=====第二部=====')
+	xls_concat(path_new)
+	print('=====结束=====')
