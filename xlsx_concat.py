564f8be4fea7082bc795af43a2bc73e50127f62f
Keating <keating911@outlook.com>
2020-06-04 22:10:29 +0800

    Update xlsx_concat.py

--- xlsx_concat.py
+++ xlsx_concat.py
@@ -1,96 +1,68 @@
 # -*- coding: utf-8 -*-
-#1	将xls和csv转化为xlsx		Trans(path)
-#2	将xlsx合并（按列）		xlsx_concat(path_new)
-#3	1、2合并				xlsx_con()
-
 import win32com.client as win32
-import os 
-import pandas as pd 
+import os
+import pandas as pd
 import xlrd
 
-def help():
-	help_o=pd.DataFrame(data=[['xls、csv转化xlsx','Trans(path)','win32,os '],
-		['xlsx合并(按列)','xlsx_concat(path_new)','pd,os,xlrd'],
-		['xls转化合并','xlsx_con','//']],
-		columns=['名称','调用','库'])
-	print(help_o)
+# 1.创建工作目录
 
+dir_path = os.getcwd()
+new_path = dir_path + '\\_Workdir\\Data'
+if os.path.exists(new_path):
+    pass
+else:
+    os.mkdirs(new_path)
 
-def Trans(path):
-	#格式转化
-	#1	创建Transfrom-xlsx文件夹	
-	path_new=path+'\\Trans'
-	if os.path.exists(path_new):
-		pass
-	else:
-		os.mkdir(path_new)
-	#2	获取xls、csv、xlsx文件名列表
-	print('工作目录',path)
-	spl_list=['.xls','.csv','.xlsx']
-	filelist = []
-	for root,dirs,files in os.walk(path):
-	    for file in files:
- 	       if os.path.splitext(file)[1] in spl_list:
- 	           filelist.append(file)
-	aa=len(filelist)
-	print('需要转化 %d 个文档:'%(aa))
-	print('文件列表',filelist)
-	print('转化中......')
-	#3	转化格式为xlsx，并保存到Transfrom_xlsx
-	excel = win32.gencache.EnsureDispatch('Excel.Application')
-	for filelist_new in filelist:
-		fff=path+'\\'+filelist_new
-		wb = excel.Workbooks.Open(fff)
-		wb.SaveAs(path_new+'\\'+filelist_new+".xlsx", FileFormat = 51)    #FileFormat = 51 is for .xlsx extension
-		wb.Close()                               #FileFormat = 56 is for .xls extension
-	excel.Application.Quit()
-	print('转化结束')	
+# 2.读取文件名
 
+file_list = []
+file_name_list = []
+spl_list = ['.xlsx', '.csv', '.xls']
+for root, dirs, files in os.walk(new_path):
+    for file in files:
+        if os.path.splitext(file)[1] in spl_list:
+            file_list.append(file)
+            file_name_list.append(os.path.splitext(file)[0])
+file_sum = len(file_name_list)
+print('共有%d个文件')%(file_sum)
+print('正在格式化')
 
-def xlsx_concat(path_new):
-	#合并
-	#1	获取xlsx文件名称列表
-	print('合并目录',path_new)
-	filelist = []
-	spl_list=['.xlsx']
-	for root,dirs,files in os.walk(path_new):
-	    for file in files:
-	        if os.path.splitext(file)[1] in spl_list:
- 	           filelist.append(file)
-	aa=len(filelist)
-	print('需合并 %d 个文档:'%(aa))
-	print('文件列表',filelist)
-	print('合并中......')
-	#2	获取表及sheet名称列表	
-	Sheetlist=[]
-	datalist = []
-	for num_file in range(aa):
-		file_n = path_new + '/' + filelist[num_file]
-		wb =xlrd.open_workbook(file_n)
-		sheet_name_1= wb.sheet_names()
-		sheetsum= len(sheet_name_1)
-	#3	将excel数据以Datafram装入datalist列表
-		for sheet_new in sheet_name_1:
-			datalist.append(pd.read_excel(file_n,sheet_name=sheet_new))
-			data_last=datalist[-1]
-			col_name=data_last.columns.tolist()
-			col_name.insert(0,'来源')
-			data_last=data_last.reindex(columns=col_name)
-			data_last['来源'] = str(filelist[num_file])+'@'+str(sheet_new)
-	#4	将datalist的所有DataFrame按列合并
-	data = pd.concat(datalist,axis=0,sort=False)
-	outwriter = path_new  + '/' + '@excel_output.xls'
-	data.to_excel(outwriter)
-	print('合并成功')
-	print('已输出到Trans/@excel_output.xls')
-	print(data.shape)
+# 3.文件格式化
 
+excel = win32.gencache.EnsureDispatch('Excel.Application')
+for file_n in file_list:
+    file_path = dir_path + '\\' + file_n
+    wb = excel.Workbooks.Open(file_path)
+    file_name = os.path.splitext(file_n)[0]
+    wb.SaveAs(new_path + '\\' + file_name + ".xlsx",
+              FileFormat=51)  #FileFormat = 51 is for .xlsx extension
+    wb.Close()  #FileFormat = 56 is for .xls extension
+excel.Application.Quit()
+print('格式化结束')
 
-def xlsx_con():
-	path=os.getcwd()
-	path_new=path+'\\Trans'
-	print('======1/2======')
-	Trans(path)
-	print('======2/2======')
-	xlsx_concat(path_new)
-	print('======结束======')
+# 4.合并文件
+print('开始合并文件')
+
+Sheet_list = []
+data_list = []
+for n in range(file_sum):
+    file_n = new_path + '/' + file_name_list[n] + '.xlsx'
+    wb = xlrd.open_workbook(file_n)
+    sheet_name_list = wb.sheet_names()
+    sheet_sum = len(sheet_name_list)
+    
+    for sheet_n in range(sheet_sum):
+        data_list.append(
+            pd.read_excel(file_n, sheet_name=sheet_name_list[sheet_n]))
+        data_last = data_list[-1]
+        col_name = data_last.columns.tolist()
+        col_name.insert(0, '来源')
+        data_last = data_last.reindex(columns=col_name)
+        data_last['来源'] = str(n) + '-' + str(sheet_n + 1) + str(
+            file_name_list[n]) + '@' + str(sheet_new)
+
+data = pd.concat(data_list, axis=0, sort=False)
+output_file = dir_path + '\\_Workdir\\@excel_output.xlsx'
+data.to_excel(output_file)
+print('合并结束，文件输出到%s')%(output_file)
+print('datashape:',data.shape)
